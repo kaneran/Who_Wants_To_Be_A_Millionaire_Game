f21c8fb7cbb63b565c89cff209be5642eae480a0
Kane <95705546+kaneran@users.noreply.github.com>
2022-10-23 12:46:42 +0100

    Created high-level layout using flexbox

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const button = document.querySelector("#hi");
-button.addEventListener("click", () =>{
-    console.log("Hello!");
-});
-
 const prizes = ['£100', '£200', '£300', '£500', '£1,000', '£2,000', '£4,000', 
 '£8,000', '£16,000', '£32,000', '£64,000', '£125,000', '£250,000', '£500,000', '£1 MILLION'];
 const questionPrizeMap = prizes.map((prize, index) => ({questionNumber: index, questionAnsweredCorrectly: false, prize}));
@@ -13,4 +8,9 @@ questionPrizeMap.forEach(({questionNumber, prize}) => {
     const question = document.createElement("p");
     question.innerText = `${questionNo} ${formattedPrize}`;
     progress.appendChild(question);
+});
+
+const phoneButton = document.querySelector('#phone');
+phoneButton.addEventListener("click", () => {
+    console.log("Calling friend....");
 })
