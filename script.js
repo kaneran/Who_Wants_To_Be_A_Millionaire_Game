5e03e92ce6a00a0940b82054185175f886161902
Kane <95705546+kaneran@users.noreply.github.com>
2022-10-26 21:23:49 +0100

    CSS styling part 4

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
+const currentQuestionNo = 1;
 const prizes = ['£100', '£200', '£300', '£500', '£1,000', '£2,000', '£4,000', 
 '£8,000', '£16,000', '£32,000', '£64,000', '£125,000', '£250,000', '£500,000', '£1 MILLION'];
 const questionPrizeMap = prizes.map((prize, index) => ({questionNumber: index, questionAnsweredCorrectly: false, prize}));
 const progress = document.querySelector("#progress");
 questionPrizeMap.forEach(({questionNumber, prize}) => {
     const questionNo = questionNumber + 1;
+    const doApplyHighlighting = questionNo === currentQuestionNo;
     const formattedPrize = prize.replace('Â', '');
     const question = document.createElement("div");
     const [index, amount] = [document.createElement("span"),document.createElement("span")];
     index.innerText = questionNo;
     amount.innerText = formattedPrize;
+    if(doApplyHighlighting){
+        index.style.color = '#F6F2DA';
+        question.classList.add("highlight");
+    } else{
+        question.style.color =  (questionNo % 5 === 0 ? '#f2e9a7' : '#f29435');
+    }
     question.appendChild(index);
     question.appendChild(amount);
-    question.style.color = questionNo % 5 === 0 ? '#f2e9a7' : '#f29435';
     progress.appendChild(question);
 });
 
