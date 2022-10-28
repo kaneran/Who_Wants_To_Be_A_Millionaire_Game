a68deb38cb3cad99ac2767d22f254e8edec2d45f
kanedharan <95705546+kaneran@users.noreply.github.com>
2022-10-28 17:20:15 +0100

    Render api response into gamebooard

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+let currentQuestionNo = 1;
 const populateProgressSection = () => {
-    let currentQuestionNo = 1;
     const prizes = ['£100', '£200', '£300', '£500', '£1,000', '£2,000', '£4,000',
         '£8,000', '£16,000', '£32,000', '£64,000', '£125,000', '£250,000', '£500,000', '£1 MILLION'];
     const questionPrizeMap = prizes.map((prize, index) => ({ questionNumber: index, questionAnsweredCorrectly: false, prize }));
@@ -30,20 +30,44 @@ phoneButton.addEventListener("click", () => {
 
 });
 
+let allQuestions = [];
 
 const getQuestions = (difficulty) => {
-    const request = new XMLHttpRequest();
-    const url = `https://opentdb.com/api.php?amount=5&difficulty=${difficulty}&type=multiple`;
-    request.open("GET", url);
-    request.send();
+    return fetch(`https://opentdb.com/api.php?amount=5&difficulty=${difficulty}&type=multiple`).then((data) => data.json()).then((questions) => {
+        allQuestions.push(...questions.results);
+    });
+}
+const questionSection = document.querySelector("#question");
+getQuestions("easy")
+.then(() => getQuestions("medium"))
+.then(() => getQuestions("hard")).then(() => {
+    beginGame();
+});
 
-    request.onreadystatechange = (e) => {
-        const { results } = JSON.parse(request.responseText);
-        console.log(results);
-    }
-    request
+const beginGame = () => {
+    const questionNo = currentQuestionNo - 1;
+    const {question, correct_answer, incorrect_answers} = allQuestions[questionNo];
+    questionSection.innerText = question.replace(/&quot;/g, "'");
+    const answers = [...incorrect_answers, correct_answer];
+    const shuffledAnswers = shuffleAnswers(answers);
+    populatesAnswers(shuffledAnswers);
 }
 
-getQuestions("easy");
-getQuestions("medium");
-getQuestions("hard");
+//Fun-fact, this is called the Fisher-Yates Shuffle algorithm!
+const shuffleAnswers = (answers) => {
+    for(let i = answers.length -1; i > 0 ;i--){
+        let j = Math.floor(Math.random() * (i+1));
+        [answers[i], answers[j]] = [answers[j], answers[i]];
+    }
+    return answers;
+}
+
+const populatesAnswers = (answers) => {
+    const answerSections = document.querySelectorAll('#answer');
+    answerSections.forEach((answerSection, index) => {
+        answerSection.innerText = answers[index];
+    });
+}
+
+
+
